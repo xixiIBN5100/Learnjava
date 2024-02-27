28b6756b4112b2c5a67acc805e01f5b7cec4a42a
xixiibn5100 <1584914306@qq.com>
2024-02-27 16:43:17 +0800

    test

--- src/test_01/GirlFriend.java
+++ src/test_01/GirlFriend.java
@@ -1,51 +1,15 @@
 package test_01;
 
 public class GirlFriend {
-    private String name;
-    private int age;
+    String name;
+    int age;
 
-
-    public GirlFriend() {
+    public void sayHellow(){
+        System.out.println("hello word");
     }
 
-    public GirlFriend(String name, int age) {
-        this.name = name;
-        this.age = age;
+    public void sayGoofbye(){
+        System.out.println("Goodbye");
     }
 
-    /**
-     * 获取
-     * @return name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * 设置
-     * @param name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * 获取
-     * @return age
-     */
-    public int getAge() {
-        return age;
-    }
-
-    /**
-     * 设置
-     * @param age
-     */
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String toString() {
-        return "GirlFriend{name = " + name + ", age = " + age + "}";
-    }
 }
--- src/test_01/MyGirlfriend.java
+++ src/test_01/MyGirlfriend.java
@@ -3,6 +3,10 @@ package test_01;
 public class MyGirlfriend {
     public static void main(String[] args) {
         GirlFriend girlFriend_01 = new GirlFriend();
-
+        girlFriend_01.age = 18;
+        girlFriend_01.sayGoofbye();
+        girlFriend_01.sayHellow();
+        System.out.println(girlFriend_01.age);
+        System.out.println(girlFriend_01.name);
     }
 }
