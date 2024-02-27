9a95a9e375f909a92618d67f968f865c7c263768
xixiibn5100 <1584914306@qq.com>
2024-02-27 15:34:52 +0800

    first commit

---
+++ src/test_01/GirlFriend.java
@@ -0,0 +1,15 @@
+package test_01;
+
+public class GirlFriend {
+    String name;
+    int age;
+
+    public void sayHellow(){
+        System.out.println("hello word");
+    }
+
+    public void sayGoofbye(){
+        System.out.println("Goodbye");
+    }
+
+}
